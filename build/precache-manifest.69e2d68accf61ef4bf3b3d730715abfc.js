06f030f0c80e24c25beeb06009f88e21c22f400d
ask-nugey <mischief.moi@gmail.com>
2023-09-10 01:08:24 +0900

    build

---
+++ build/precache-manifest.69e2d68accf61ef4bf3b3d730715abfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ae780b61b84fee795a3",
+    "url": "/static/css/main.6c16e012.chunk.css"
+  },
+  {
+    "revision": "0ae780b61b84fee795a3",
+    "url": "/static/js/main.0ae780b6.chunk.js"
+  },
+  {
+    "revision": "6b130bdd756e2cb94f81",
+    "url": "/static/js/1.6b130bdd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5dd85998d199420812cf1d38c1e18a7a",
+    "url": "/index.html"
+  }
+];
